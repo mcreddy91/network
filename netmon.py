1bf4b8336009f38c5888afd8c66cf5f7a15fb351
David Lawrence <dlaw@dlaw.me>
2011-11-17 15:20:15 -0500

    Added simple script to show top 5 users.

--- netmon.py
+++ netmon.py
@@ -7,10 +7,15 @@ t = time.time()
 now = stats.current_stats()
 
 while True:
-    time.sleep(t + sleeptime - time.time())
-    t = time.time()
-    then, now = now, stats.current_stats()
-    result = stats.subtract_stats(now, then)
-    for mac in result:
-        print(mac)
-        print(result[mac])
+    try:
+        time.sleep(t + sleeptime - time.time())
+        t = time.time()
+        then, now = now, stats.current_stats()
+        result = stats.subtract_stats(now, then)
+        macs = sorted(result.keys(), key = lambda mac: -sum(result[mac]['traffic']))
+        for mac in macs[:5]:
+            rcvd, sent = [octets/(sleeptime * 2**17) for octets in result[mac]['traffic']]
+            print('{0}\t{1}\t{2:.3} mb/s\t{3:.3} mb/s'.format(mac, result[mac]['ip'], rcvd, sent))
+    except KeyboardInterrupt:
+        break
+        
